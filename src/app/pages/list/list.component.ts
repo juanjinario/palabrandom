36452245a8e45c44bec1fec0b4d8b7e5c0b88aa3
juanjinario <juanjco2@hotmail.com>
2020-04-25 19:36:15 +0200

    Anadir servicio lista random

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -8,25 +8,24 @@ import { Observable } from 'rxjs';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent {
-  $allWords: Observable<any[]>;
-  numberOptions: {}[];
-  number: {};
+  words$: any;
+  numberOptions: any[];
+  number: any;
   wordList;
 
   constructor(private wordsService: WordsService) {
     this.numberOptions = [
       {id: 1, title: '1 palabra', value: 1},
-      {id: 5, title: '5 palabra', value: 5},
-      {id: 20, title: '20 palabra', value: 20},
-      {id: 50, title: '50 palabra', value: 50},
-      {id: 100, title: '100 palabra', value: 100},
-      {id: 200, title: '200 palabra', value: 200},
+      {id: 5, title: '5 palabras', value: 5},
+      {id: 20, title: '20 palabras', value: 20},
+      {id: 50, title: '50 palabras', value: 50},
+      {id: 100, title: '100 palabras', value: 100},
+      {id: 200, title: '200 palabras', value: 200},
     ];
     this.number = this.numberOptions[0];
   }
 
   getWords() {
-    this.$allWords = this.wordsService.getWords();
-    console.log(this.$allWords);
+    this.words$ = this.wordsService.getWords(this.number.value);
   }
 }
