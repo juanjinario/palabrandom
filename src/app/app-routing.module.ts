1aaf1d5f04ac106ba1eccb62f0d841aeb40bee78
juanjinario <juanjco2@hotmail.com>
2020-04-25 13:26:10 +0200

    Anadir pagina de lista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   { path: 'upload', loadChildren: () => import('./pages/upload/upload.module').then(m => m.UploadModule) },
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'list', loadChildren: () => import('./pages/list/list.module').then(m => m.ListModule) },
+  { path: '', pathMatch: 'full', redirectTo: 'list' },
 ];
 
 @NgModule({
